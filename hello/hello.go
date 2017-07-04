e096912b6f2089a579aa9c56cddca95ff8cc1bdc
Hongkai Liu <hongkailiu@users.noreply.github.com>
2017-07-04 15:20:12 -0400

    go lib and test

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "github.com/hongkailiu/test-go/stringutil"
+)
 
 func main() {
-    fmt.Printf("hello, world\n")
+    //fmt.Printf("hello, world\n")
+    fmt.Printf(stringutil.Reverse("!oG ,olleH"))
 }
