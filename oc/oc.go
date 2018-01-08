e7510bd68df55d578fffb3edeec652982b2d1a6b
Hongkai Liu <hongkailiu@users.noreply.github.com>
2018-01-08 09:27:45 -0500

    bc

--- oc/oc.go
+++ oc/oc.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    v1 "github.com/openshift/api/build/v1"
     "k8s.io/apimachinery/pkg/api/errors"
     metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
     buildv1 "github.com/openshift/client-go/build/clientset/versioned/typed/build/v1"
@@ -31,28 +32,48 @@ func main() {
         panic(err.Error())
     }
 
-    builds, err := buildV1Client.Builds("").List(metav1.ListOptions{})
+    buildConfigs, err := buildV1Client.BuildConfigs("").List(metav1.ListOptions{})
 
     if err != nil {
         panic(err.Error())
     }
-    fmt.Printf("There are %d builds in the cluster\n", len(builds.Items))
+    fmt.Printf("There are %d builds in the cluster\n", len(buildConfigs.Items))
 
     //Change namespace and build accordingly
-    namespace := "testkkk"
-    build := "cakephp-mysql-example-1"
-    myBuild, err := buildV1Client.Builds(namespace).Get(build, metav1.GetOptions{})
+    namespace := "testproject"
+    buildConfig := "cakephp-ex"
+    myBuildConfig, err := buildV1Client.BuildConfigs(namespace).Get(buildConfig, metav1.GetOptions{})
     if errors.IsNotFound(err) {
-        fmt.Printf("Build %s in namespace %s not found\n", build, namespace)
+        fmt.Printf("Build %s in namespace %s not found\n", buildConfig, namespace)
     } else if statusError, isStatus := err.(*errors.StatusError); isStatus {
         fmt.Printf("Error getting pod %s in namespace %s: %v\n",
-            build, namespace, statusError.ErrStatus.Message)
+            buildConfig, namespace, statusError.ErrStatus.Message)
     } else if err != nil {
         panic(err.Error())
     } else {
-        fmt.Printf("Found build %s in namespace %s\n", build, namespace)
-        fmt.Printf("Found build %s in status %+v\n", build, myBuild.Status)
+        fmt.Printf("Found build %s in namespace %s\n", buildConfig, namespace)
+        fmt.Printf("Found build %s in status %+v\n", buildConfig, myBuildConfig.Status)
     }
+
+    buildRequest := v1.BuildRequest{}
+    buildRequest.Kind = "BuildRequest"
+    buildRequest.APIVersion = "build.openshift.io/v1"
+    objectMeta := metav1.ObjectMeta{}
+    objectMeta.Name = "cakephp-ex"
+    buildRequest.ObjectMeta = objectMeta
+    buildTriggerCause := v1.BuildTriggerCause{}
+    buildTriggerCause.Message = "Manually triggered"
+    buildRequest.TriggeredBy = []v1.BuildTriggerCause{buildTriggerCause}
+    myBuild, err := buildV1Client.BuildConfigs(namespace).Instantiate(buildConfig, &buildRequest)
+
+    if err != nil {
+        panic(err.Error())
+    } else {
+        fmt.Printf("====Trigger build %s in namespace %s\n", myBuild.Name, myBuild.Namespace)
+        //fmt.Printf("Found build %s in status %+v\n", buildConfig, myBuildConfig.Status)
+    }
+
+
 }
 
 func homeDir() string {
