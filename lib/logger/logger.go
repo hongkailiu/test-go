3222714f31c3a26bf2e6b5573bb340e06676a2b2
Hongkai Liu <hongkailiu@users.noreply.github.com>
2018-02-04 17:03:26 -0500

    Add logrus hook to write to syslog

--- lib/logger/logger.go
+++ lib/logger/logger.go
@@ -1,8 +1,10 @@
 package logger
 
 import (
+	"log/syslog"
 	"os"
 	"github.com/sirupsen/logrus"
+	lSyslog "github.com/sirupsen/logrus/hooks/syslog"
 )
 
 var Logger = logrus.New()
@@ -11,6 +13,14 @@ func init() {
 	Logger.Formatter = &logrus.TextFormatter{FullTimestamp:true}
 	Logger.Out = os.Stdout
 	Logger.Level = logrus.DebugLevel
+
+	hook, err := lSyslog.NewSyslogHook("", "", syslog.LOG_INFO, "")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	Logger.Hooks.Add(hook)
 }
 
 
