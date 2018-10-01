ff02c003aeae289ac253b8d06f62640f7040a2de
Hongkai Liu <hongkailiu@users.noreply.github.com>
2018-10-01 08:26:02 -0400

    Debug on travis 2

--- script/ci/build-flexy.sh
+++ script/ci/build-flexy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 rm -fv flexy/flexy.test
+rm -fv bulid/flexy.test
 ginkgo build flexy/
-mv flexy/flexy.test build
+mv -v flexy/flexy.test ./build/
 
 #TO run
 # ginkgo -v -focus="\[Main\] Flexy" ./build/flexy.test
