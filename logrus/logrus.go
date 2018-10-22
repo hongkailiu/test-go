285a7a1ba549efaf5558ab1059c0c99006ed84b6
Hongkai Liu <hongkailiu@users.noreply.github.com>
2018-10-22 18:03:31 -0400

    Update logrus: tuning format tty or not

--- logrus/logrus.go
+++ logrus/logrus.go
@@ -1,27 +1,22 @@
 package main
 
 import (
-	log "github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 	"os"
 )
 
-func init() {
-
-	log.SetFormatter(&log.TextFormatter{FullTimestamp: true,})
-
-	// Log as JSON instead of the default ASCII formatter.
-	//log.SetFormatter(&log.JSONFormatter{})
-
-	// Output to stdout instead of the default stderr
-	// Can be any io.Writer, see below for File example
-	log.SetOutput(os.Stdout)
-
-	// Only log the warning severity or above.
-	//log.SetLevel(log.WarnLevel)
-}
 
 func main() {
-	log.WithFields(log.Fields{
+	//https://github.com/sirupsen/logrus/blob/master/example_basic_test.go
+	var log = logrus.New()
+	log.Formatter = new(logrus.TextFormatter)
+	log.Out = os.Stdout
+	log.Formatter.(*logrus.TextFormatter).DisableColors = true    // remove colors
+	//log.Formatter.(*logrus.TextFormatter).DisableTimestamp = true // remove timestamp from test output
+
+
+	log.WithFields(logrus.Fields{
 		"animal": "walrus",
 	}).Info("A walrus appears")
+
 }
