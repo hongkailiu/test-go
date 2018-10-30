681c92de46bd6bf0d4d3f685ca4862fa40cce24c
Hongkai Liu <hongkailiu@users.noreply.github.com>
2018-10-30 17:33:45 -0400

    Add google login

--- http/server.go
+++ http/server.go
@@ -3,6 +3,7 @@ package http
 import (
 	"context"
 	"fmt"
+	"golang.org/x/oauth2/google"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -21,6 +22,7 @@ import (
 	log "github.com/sirupsen/logrus"
 	"golang.org/x/oauth2"
 	githuboauth "golang.org/x/oauth2/github"
+	googleauth "google.golang.org/api/oauth2/v2"
 )
 
 var (
@@ -36,6 +38,14 @@ var (
 	}
 	// random string for oauth2 API calls to protect against CSRF
 	oauthStateString = uuid.NewV4().String()
+
+	oauthConfGoogle = &oauth2.Config{
+		ClientID:     os.Getenv("gg_client_id"),
+		ClientSecret: os.Getenv("gg_client_secret"),
+		RedirectURL:  "http://127.0.0.1:8080/google_oauth_cb",
+		Scopes:       []string{"profile", "email"},
+		Endpoint:     google.Endpoint,
+	}
 )
 
 // PrometheusLogger intercepts all http requests and logging the path
@@ -52,6 +62,7 @@ func PrometheusLogger() gin.HandlerFunc {
 func Run() {
 
 	log.WithFields(log.Fields{"oauthConf.ClientID": oauthConf.ClientID, "oauthConf.ClientSecret": oauthConf.ClientSecret}).Debug("oauthConf")
+	log.WithFields(log.Fields{"oauthConfGoogle.ClientID": oauthConfGoogle.ClientID, "oauthConfGoogle.ClientSecret": oauthConfGoogle.ClientSecret}).Debug("oauthConf")
 
 	prometheusRegister()
 
@@ -133,10 +144,55 @@ func Run() {
 			c.Redirect(http.StatusTemporaryRedirect, "/console")
 			return
 		}
-		fmt.Printf("Logged in as GitHub user: %s\n", *user.Login)
+		fmt.Printf("Logged in as GitHub user: %s\n", *user.Name)
 		session := sessions.Default(c)
 		session.Options(options)
-		session.Set("username", *user.Login)
+		session.Set("username", *user.Name)
+		session.Save()
+		c.Redirect(http.StatusTemporaryRedirect, "/console")
+	})
+
+	//https://github.com/dghubble/gologin/blob/master/google/login.go
+	r.GET("/login_google", func(c *gin.Context) {
+		url := oauthConfGoogle.AuthCodeURL(oauthStateString, oauth2.AccessTypeOnline)
+		log.WithFields(log.Fields{"url": url}).Debug("redirect login url")
+		c.Redirect(http.StatusTemporaryRedirect, url)
+	})
+
+	r.GET("/google_oauth_cb", func(c *gin.Context) {
+		r := c.Request
+		state := r.FormValue("state")
+		if state != oauthStateString {
+			fmt.Printf("invalid oauth state, expected '%s', got '%s'\n", oauthStateString, state)
+			c.Redirect(http.StatusTemporaryRedirect, "/console")
+			return
+		}
+
+		code := r.FormValue("code")
+		token, err := oauthConfGoogle.Exchange(oauth2.NoContext, code)
+		if err != nil {
+			fmt.Printf("oauthConf.Exchange() failed with '%s'\n", err)
+			c.Redirect(http.StatusTemporaryRedirect, "/console")
+			return
+		}
+
+		oauthClient := oauthConfGoogle.Client(oauth2.NoContext, token)
+		googleService, err := googleauth.New(oauthClient)
+		if err != nil {
+			fmt.Printf("googleauth.New(oauthClient) faled with '%s'\n", err)
+			c.Redirect(http.StatusTemporaryRedirect, "/console")
+			return
+		}
+		userinfoplus, err := googleService.Userinfo.Get().Do()
+		if err != nil {
+			fmt.Printf("googleService.Userinfo.Get().Do() faled with '%s'\n", err)
+			c.Redirect(http.StatusTemporaryRedirect, "/console")
+			return
+		}
+		fmt.Printf("Logged in as google user: %s\n", (*userinfoplus).Name)
+		session := sessions.Default(c)
+		session.Options(options)
+		session.Set("username", (*userinfoplus).Name)
 		session.Save()
 		c.Redirect(http.StatusTemporaryRedirect, "/console")
 	})
