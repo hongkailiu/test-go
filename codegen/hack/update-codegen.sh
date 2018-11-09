57bbc23a92120710748b729811f13efcbec1fd6a
Hongkai Liu <hongkailiu@users.noreply.github.com>
2018-11-08 23:09:23 -0500

    Go get code-gen for building

--- codegen/hack/update-codegen.sh
+++ codegen/hack/update-codegen.sh
@@ -4,10 +4,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/../..
-CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}/; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
+CODEGEN_PKG=${GOPATH}/src/k8s.io/code-generator
 
-echo "SCRIPT_ROOT: ${SCRIPT_ROOT}"
 echo "CODEGEN_PKG: ${CODEGEN_PKG}"
 
 ${CODEGEN_PKG}/generate-groups.sh all github.com/hongkailiu/test-go/codegen/pkg/client github.com/hongkailiu/test-go/codegen/pkg/apis "app:v1alpha1"
