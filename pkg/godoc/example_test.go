52de074c6738f8a18151f5cd11f8ae710e996b47
Hongkai Liu <hongkailiu@users.noreply.github.com>
2018-11-15 14:57:23 -0500

    Try more godoc c

---
+++ pkg/godoc/example_test.go
@@ -0,0 +1,17 @@
+package godoc_test
+
+import (
+	"fmt"
+	"github.com/hongkailiu/test-go/pkg/godoc"
+)
+
+func ExampleNewNiceHello_Cool() {
+	//More info https://github.com/fluhus/godoc-tricks/blob/master/doc.go
+	godoc.NewNiceHello()
+	fmt.Println("It is cool")
+}
+
+func ExampleNewNiceHello_Warm() {
+	godoc.NewNiceHello()
+	fmt.Println("It is warm")
+}
--- pkg/godoc/godoc_test.go
+++ pkg/godoc/godoc_test.go
@@ -1,7 +1,6 @@
 package godoc_test
 
 import (
-	"fmt"
 	"testing"
 
 	. "github.com/hongkailiu/test-go/pkg/godoc"
@@ -19,15 +18,4 @@ func TestNewNiceHello(t *testing.T) {
 	if err != nil {
 		t.Errorf("error should not have occurred: %s", err.Error())
 	}
-}
-
-func ExampleNewNiceHello_Cool() {
-	//More info https://github.com/fluhus/godoc-tricks/blob/master/doc.go
-	NewNiceHello()
-	fmt.Println("It is cool")
-}
-
-func ExampleNewNiceHello_Warm() {
-	NewNiceHello()
-	fmt.Println("It is warm")
 }
