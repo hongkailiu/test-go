2f7fcd78451458c8b1aa7e671c5d668f1bd1ad79
Hongkai Liu <hongkailiu@users.noreply.github.com>
2018-11-15 14:04:37 -0500

    Add pkg/doc/

---
+++ pkg/doc/hello.go
@@ -0,0 +1,12 @@
+package doc
+
+//Deprecated: NewHello is defined here to show
+//the usage for `Deprecated` in goDoc
+func NewHello() error {
+	return nil
+}
+
+//NewNiceHello says hello nicely.
+func NewNiceHello() error {
+	return nil
+}
--- pkg/hello/hello.go
+++ pkg/hello/hello.go
@@ -12,6 +12,8 @@ func main() {
 
 //Deprecated: NewHello is defined here to show
 //the usage for `Deprecated` in goDoc
+//Hoevre, it does not show in goDoc because it is in main pkg.
+//https://stackoverflow.com/questions/21778556/what-steps-are-needed-to-document-package-main-in-godoc
 func NewHello() error {
 	return nil
 }
