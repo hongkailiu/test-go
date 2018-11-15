701f7b6bd343310a573012de3704cce67e403a85
Hongkai Liu <hongkailiu@users.noreply.github.com>
2018-11-15 15:48:32 -0500

    More try with godoc 2

--- pkg/doc/example_test.go
+++ pkg/doc/example_test.go
@@ -9,15 +9,13 @@ func Example() {
 	fmt.Println("It is pkg level")
 }
 
-func ExampleStart() {
-	fmt.Println("it started!")
-	// Output: it started!
-}
 
 func ExampleStart_Warm() {
 	fmt.Println("it started warm!")
+	// Output: it started warm!
 }
 
 func ExampleStart_Cold() {
 	fmt.Println("it started cold!")
+	// Output: it started cold!
 }
