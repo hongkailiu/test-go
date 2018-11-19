3b3ced1c72f64a118fa5d55be694ba3791ad80cf
Hongkai Liu <hongkailiu@users.noreply.github.com>
2018-11-19 14:47:53 -0500

    Add examples for testify and mockery

---
+++ pkg/test/mockery/service/greeter.go
@@ -0,0 +1,54 @@
+package service
+
+type db struct{}
+
+// DB is fake Database interface.
+type DB interface {
+	FetchMessage(lang string) (string, error)
+	FetchDefaultMessage() (string, error)
+}
+
+type Greeter struct {
+	Database DB
+	Lang     string
+}
+
+// GreeterService is service to greet your friends.
+type GreeterService interface {
+	Greet() string
+	GreetInDefaultMsg() string
+}
+
+func (d *db) FetchMessage(lang string) (string, error) {
+	// in real life, this code will call an external db
+	// but for this sample we will just return the hardcoded example value
+	if lang == "en" {
+		return "hello", nil
+	}
+	if lang == "es" {
+		return "holla", nil
+	}
+	return "bzzzz", nil
+}
+
+func (d *db) FetchDefaultMessage() (string, error) {
+	return "default message", nil
+}
+
+func (g Greeter) Greet() string {
+	msg, _ := g.Database.FetchMessage(g.Lang) // call Database to get the message based on the Lang
+	return "Message is: " + msg
+}
+
+func (g Greeter) GreetInDefaultMsg() string {
+	msg, _ := g.Database.FetchDefaultMessage() // call Database to get the default message
+	return "Message is: " + msg
+}
+
+func NewDB() DB {
+	return new(db)
+}
+
+func NewGreeter(db DB, lang string) GreeterService {
+	return Greeter{db, lang}
+}
