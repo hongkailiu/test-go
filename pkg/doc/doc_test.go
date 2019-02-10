9bbd85f551e49b15d62410d3c8a3cbde72234928
Hongkai Liu <hongkailiu@users.noreply.github.com>
2019-02-09 21:53:47 -0500

    Add test pb/others

---
+++ pkg/doc/doc_test.go
@@ -0,0 +1,21 @@
+package doc_test
+
+import (
+	"testing"
+
+	. "github.com/hongkailiu/test-go/pkg/doc"
+)
+
+func TestNewHello(t *testing.T) {
+	err := NewHello()
+	if err != nil {
+		t.Errorf("error should not have occurred: %s", err.Error())
+	}
+}
+
+func TestNewNiceHello(t *testing.T) {
+	err := MYNewNiceHello()
+	if err != nil {
+		t.Errorf("error should not have occurred: %s", err.Error())
+	}
+}
---
+++ pkg/doc/example_test.go
@@ -0,0 +1,34 @@
+package doc_test
+
+import (
+	"fmt"
+	"github.com/hongkailiu/test-go/pkg/doc"
+)
+
+func Example() {
+	// http://elliot.land/post/godoc-tips-tricks
+	// those Example...() functions are also tests
+	// https://blog.golang.org/examples
+	fmt.Println("It is pkg level")
+	// Output: It is pkg level
+}
+
+func ExampleMYNewNiceHello_canada() {
+	doc.MYNewNiceHello()
+	fmt.Println("new hello canada")
+}
+
+func ExampleStart() {
+	fmt.Println("it started a function!")
+	// Output: it started a function!
+}
+
+func ExampleStart_warm() {
+	fmt.Println("it started warm!")
+	// Output: it started warm!
+}
+
+func ExampleStart_cold() {
+	fmt.Println("it started cold!")
+	// Output: it started cold!
+}
