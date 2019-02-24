520a3d8186f09e1ce5bc235178ae8cf11489347d
Hongkai Liu <hongkailiu@users.noreply.github.com>
2019-02-24 17:12:45 -0500

    Add COVERALLS_TOKEN

--- script/ci/coveralls.sh
+++ script/ci/coveralls.sh
@@ -5,21 +5,15 @@ if [[ $(echo "${CI}" | awk '{print tolower($0)}') != "true" ]]; then
   exit 1
 fi
 
-ci_service="unknown"
+go get -u github.com/mattn/goveralls
 
 if [[ $(echo "${TRAVIS}" | awk '{print tolower($0)}') == "true" ]]; then
-  ci_service="travis-ci"
+  "${GOPATH}/bin/goveralls" -coverprofile=build/coverage.out -service=travis-ci
 fi
 
 if [[ $(echo "${CIRCLECI}" | awk '{print tolower($0)}') == "true" ]]; then
-  ci_service="circle-ci"
+  ##https://github.com/lemurheavy/coveralls-public/issues/632
+  "${GOPATH}/bin/goveralls" -coverprofile=build/coverage.out -service=circle-ci -repotoken="${COVERALLS_TOKEN}"
 fi
 
-if [[ ${ci_service} == "unknown" ]]; then
-  echo "only travis ci and circle ci are supported for now"
-  exit 1
-fi
 
-go get -u github.com/mattn/goveralls
-
-"${GOPATH}/bin/goveralls" -coverprofile=build/coverage.out -service "${ci_service}"
