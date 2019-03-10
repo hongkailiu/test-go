7a6fc92959d220909759dcf26c61705c2fc9e8a1
Hongkai Liu <hongkailiu@users.noreply.github.com>
2019-03-10 18:32:01 -0400

    Install bazel on circle

--- script/ci/install-bazel.sh
+++ script/ci/install-bazel.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-if [[ "${TRAVIS}" == "true" ]]; then
+if [[ "${TRAVIS:-false}" == "true" ]]; then
   echo "installing bazel on travis-ci ..."
   echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
   curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
@@ -12,5 +12,13 @@ if [[ "${TRAVIS}" == "true" ]]; then
   exit 0
 fi
 
-echo "not CI environment ... exit 1"
+if [[ "${CIRCLECI:-false}" == "true" ]]; then
+  echo "installing bazel on travis-ci ..."
+  echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+  curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
+  sudo apt-get update && sudo apt-get install bazel
+  exit 0
+fi
+
+echo "not supported CI environment ... exit 1"
 exit 1
