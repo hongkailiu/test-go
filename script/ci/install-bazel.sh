b0b7f7f9f55ed5814a5e8eafdecd50c1be1c4239
Hongkai Liu <hongkailiu@users.noreply.github.com>
2019-03-10 18:59:45 -0400

    Install openjdk on circle ci

--- script/ci/install-bazel.sh
+++ script/ci/install-bazel.sh
@@ -13,7 +13,11 @@ if [[ "${TRAVIS:-false}" == "true" ]]; then
 fi
 
 if [[ "${CIRCLECI:-false}" == "true" ]]; then
-  echo "installing bazel on travis-ci ..."
+  echo "installing bazel on circle-ci ..."
+  sudo add-apt-repository ppa:openjdk-r/ppa
+  sudo apt-get update
+  sudo apt-get install openjdk-8-jdk
+  echo "installing bazel on circle-ci ..."
   echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
   curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
   sudo apt-get update && sudo apt-get install bazel
