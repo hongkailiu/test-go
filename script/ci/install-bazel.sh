2dbc5772f89379424402aaec7ed9309db3424bb0
Hongkai Liu <hongkailiu@users.noreply.github.com>
2019-03-10 19:52:48 -0400

    Install patch on circle ci

--- script/ci/install-bazel.sh
+++ script/ci/install-bazel.sh
@@ -19,6 +19,8 @@ if [[ "${CIRCLECI:-false}" == "true" ]]; then
   #sudo apt-get update
   #sudo apt-get install --allow-unauthenticated oracle-java8-installer
   sudo apt-get install openjdk-8-jdk
+  echo "installing patch on circle-ci ..."
+  sudo apt-get install patch
   echo "installing bazel on circle-ci ..."
   echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
   curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
