b2c894efce85e45d00a30816a608e5a031ffa0d3
Hongkai Liu <hongkailiu@users.noreply.github.com>
2019-03-10 19:35:33 -0400

    Try with Oracle JDK

--- script/ci/install-bazel.sh
+++ script/ci/install-bazel.sh
@@ -14,10 +14,10 @@ fi
 
 if [[ "${CIRCLECI:-false}" == "true" ]]; then
   echo "installing openjdk on circle-ci ..."
-  #sudo apt-get install -y software-properties-common
-  #sudo add-apt-repository ppa:openjdk-r/ppa
-  #sudo apt-get update
-  sudo apt-get install openjdk-8-jdk
+  sudo apt-get install -y software-properties-common
+  sudo add-apt-repository -y ppa:webupd8team/java
+  sudo apt-get update
+  sudo apt-get install oracle-java8-installer
   echo "installing bazel on circle-ci ..."
   echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
   curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
