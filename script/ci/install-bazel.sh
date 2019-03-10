79bb75c124aa80a51579f78d3ce69af1b258b5c1
Hongkai Liu <hongkailiu@users.noreply.github.com>
2019-03-10 19:06:08 -0400

    Fix add-apt-repository: command not found

--- script/ci/install-bazel.sh
+++ script/ci/install-bazel.sh
@@ -13,7 +13,8 @@ if [[ "${TRAVIS:-false}" == "true" ]]; then
 fi
 
 if [[ "${CIRCLECI:-false}" == "true" ]]; then
-  echo "installing bazel on circle-ci ..."
+  echo "installing openjdk on circle-ci ..."
+  sudo apt-get install software-properties-common
   sudo add-apt-repository ppa:openjdk-r/ppa
   sudo apt-get update
   sudo apt-get install openjdk-8-jdk
