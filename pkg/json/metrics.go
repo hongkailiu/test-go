0431fefe6f7bda754b72abae0d56eecd75a80429
Hongkai Liu <hongkailiu@users.noreply.github.com>
2019-06-10 10:34:18 -0400

    Add pkg/json/

---
+++ pkg/json/metrics.go
@@ -0,0 +1,76 @@
+package json
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
+const (
+	markerName string = "cluster_loader_marker"
+)
+
+type Metrics interface {
+	printLog() error
+}
+
+type BaseMetrics struct {
+	// To let the 3rd party know that this log entry is important
+	// TODO set this up by config file
+	Marker string `json:"marker"`
+	Name   string `json:"name"`
+	Type   string `json:"type"`
+}
+
+type TestDuration struct {
+	BaseMetrics
+	StartTime    time.Time      `json:"startTime"`
+	TestDuration time.Duration  `json:"testDuration"`
+	Steps        []StepDuration `json:"Steps"`
+}
+
+type StepDuration interface {
+}
+
+type BaseStepDuration struct {
+	Type      string        `json:"type"`
+	StartTime time.Time     `json:"startTime"`
+	TotalTime time.Duration `json:"totalTime"`
+}
+
+type TemplateStepDuration struct {
+	BaseStepDuration
+	RateDelay      time.Duration `json:"rateDelay"`
+	RateDelayCount int           `json:"rateDelayCount"`
+	StepPause      time.Duration `json:"stepPause"`
+	StepPauseCount int           `json:"stepPauseCount"`
+	SyncTime       time.Duration `json:"syncTime"`
+}
+
+type PodStepDuration struct {
+	BaseStepDuration
+	WaitPodsDurations []time.Duration `json:"waitPodsDurations"`
+	RateDelay         time.Duration   `json:"rateDelay"`
+	RateDelayCount    int             `json:"rateDelayCount"`
+	StepPause         time.Duration   `json:"stepPause"`
+	StepPauseCount    int             `json:"stepPauseCount"`
+	SyncTime          time.Duration   `json:"syncTime"`
+}
+
+func (td TestDuration) printLog() error {
+	b, err := json.Marshal(td)
+	fmt.Println(string(b))
+	return err
+}
+
+func LogMetrics(metrics []Metrics) error {
+	for _, m := range metrics {
+		err := m.printLog()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+
---
+++ pkg/json/metrics_test.go
@@ -0,0 +1,33 @@
+package json
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTypeDuration(t *testing.T) {
+	objectUnderTest := 3*time.Second + 5*time.Minute
+	actual := fmt.Sprintf("%s", objectUnderTest)
+	assert.Equal(t, "5m3s", actual, "they should be equal")
+}
+
+func TestTypeDuration1(t *testing.T) {
+	td := TestDuration{
+		TestDuration: 3*time.Second + 5*time.Minute,
+	}
+
+	bolB, err := json.Marshal(td)
+	assert.Nil(t, err)
+
+	actual := string(bolB)
+
+	//fmt.Println("=" + actual + "=")
+
+	expect := `{"marker":"","name":"","type":"","startTime":"0001-01-01T00:00:00Z","testDuration":303000000000,"Steps":null}`
+
+	assert.Equal(t, expect, actual, "they should be equal")
+}
