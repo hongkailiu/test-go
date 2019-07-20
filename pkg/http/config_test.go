37aad660ad0052744fb57a9b795633f1d66096d1
Hongkai Liu <hongkailiu@users.noreply.github.com>
2019-07-20 01:19:18 -0400

    Add more tests on loadConfig

--- pkg/http/config_test.go
+++ pkg/http/config_test.go
@@ -7,22 +7,26 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var (
+	defaultSessionKey = []byte{116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116}
+)
+
 func TestLoadConfig1(t *testing.T) {
 	c := loadConfig()
-	assert.Equal(t, []byte{116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116}, c.sessionKey)
+	assert.Equal(t, defaultSessionKey, c.sessionKey)
 }
 
 func TestLoadConfig2(t *testing.T) {
 	os.Setenv("session_key", "[")
 	c := loadConfig()
-	assert.NotEqual(t, []byte{116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116}, c.sessionKey)
+	assert.NotEqual(t, defaultSessionKey, c.sessionKey)
 	assert.Equal(t, 32, len(c.sessionKey))
 }
 
 func TestLoadConfig3(t *testing.T) {
 	os.Setenv("session_key", "[116 117 118]")
 	c := loadConfig()
-	assert.NotEqual(t, []byte{116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116}, c.sessionKey)
+	assert.NotEqual(t, defaultSessionKey, c.sessionKey)
 	assert.Equal(t, 32, len(c.sessionKey))
 }
 
@@ -30,4 +34,11 @@ func TestLoadConfig4(t *testing.T) {
 	os.Setenv("session_key", "[116 117 118 116 116 116 116 116 116 116 116 116 116 116 116 116 116 116 116 116 116 116 116 116 116 116 116 116 116 116 116 116]")
 	c := loadConfig()
 	assert.Equal(t, []byte{116, 117, 118, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116}, c.sessionKey)
+}
+
+func TestLoadConfig5(t *testing.T) {
+	os.Setenv("session_key", "[116 aa 118 116 116 116 116 116 116 116 116 116 116 116 116 116 116 116 116 116 116 116 116 116 116 116 116 116 116 116 116 116]")
+	c := loadConfig()
+	assert.NotEqual(t, defaultSessionKey, c.sessionKey)
+	assert.Equal(t, 32, len(c.sessionKey))
 }
