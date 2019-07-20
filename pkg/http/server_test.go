3df5fe11e77595bc78814dd1ad5285d0aa532bb9
Hongkai Liu <hongkailiu@users.noreply.github.com>
2019-07-20 00:29:52 -0400

    Add more tests on http server

--- pkg/http/server_test.go
+++ pkg/http/server_test.go
@@ -1,9 +1,12 @@
 package http
 
 import (
+	"encoding/json"
+	"github.com/hongkailiu/test-go/pkg/http/info"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/hongkailiu/test-go/pkg/http/db"
 	cmdconfig "github.com/hongkailiu/test-go/pkg/testctl/cmd/config"
@@ -18,7 +21,7 @@ func TestRoute1(t *testing.T) {
 
 	appConfig = loadConfig()
 
-	hc := cmdconfig.HttpConfig{}
+	hc := cmdconfig.HttpConfig{Version: "test-version"}
 
 	oauthConfGitHub := &oauth2.Config{
 		ClientID:     "appConfig.ghClientID",
@@ -44,8 +47,23 @@ func TestRoute1(t *testing.T) {
 	router := setupRouter(&hc, githubLogin, googleLogin, &dbService)
 
 	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/", nil)
+	req, err := http.NewRequest("GET", "/", nil)
+	assert.Nil(t, err)
 	router.ServeHTTP(w, req)
 
 	assert.Equal(t, http.StatusOK, w.Code)
+
+	i := info.Info{}
+	json.Unmarshal([]byte(w.Body.Bytes()), &i)
+	assert.Equal(t, hc.Version, i.Version)
+	assert.NotEmpty(t, i.Ips)
+	assert.True(t, time.Now().Sub(i.Now) > 0)
+
+
+	w = httptest.NewRecorder()
+	req, err = http.NewRequest("GET", "/api/v1/users", nil)
+	assert.Nil(t, err)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusForbidden, w.Code)
 }
