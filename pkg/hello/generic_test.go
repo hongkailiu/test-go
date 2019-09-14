d8bd5fd70d2f7ecae0d9205ceac7f90d26f1a3f7
Hongkai Liu <hongkailiu@users.noreply.github.com>
2019-09-14 10:20:00 -0400

    Add test TestNewPerson

--- pkg/hello/generic_test.go
+++ pkg/hello/generic_test.go
@@ -2,9 +2,13 @@ package main_test
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	"k8s.io/apimachinery/pkg/util/diff"
+	"k8s.io/apimachinery/pkg/util/sets"
 )
 
 type Person interface {
@@ -39,7 +43,32 @@ func Test1(t *testing.T) {
 	}
 }
 
+type Dog struct {
+	ID      int
+	Friends []string
+}
+
+func NewPerson() Dog {
+	return Dog{
+		ID:      3,
+		Friends: sets.NewString().List(),
+	}
+}
+
 func TestSomething(t *testing.T) {
 	// assert equality
 	assert.Equal(t, 123, 123, "they should be equal")
+}
+
+func TestNewPerson(t *testing.T) {
+	expected := Dog{
+		ID:      3,
+		Friends: sets.NewString().List(),
+	}
+
+	result := NewPerson()
+
+	if !reflect.DeepEqual(expected, result) {
+		t.Errorf("Unexpected mis-match: %s", diff.ObjectReflectDiff(expected, result))
+	}
 }
