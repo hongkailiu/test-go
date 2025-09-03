8a56111477e65b5bbdc62a8f88404a59f604988f
Hongkai Liu <hongkailiu@users.noreply.github.com>
2025-09-03 01:38:17 -0400

    Add /healthz

--- cmd/router.go
+++ cmd/router.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/sirupsen/logrus"
@@ -12,17 +13,27 @@ func getRouter(ctx context.Context, graphService GraphService) *http.ServeMux {
 	handler := http.NewServeMux()
 
 	handler.HandleFunc("/graph", func(w http.ResponseWriter, r *http.Request) {
-
-		registry, err := graphService.Get(ctx)
+		graph, err := graphService.Get(ctx)
 		if err != nil {
 			logrus.WithError(err).Error("Error loading registry data")
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 		w.Header().Set("Content-Type", "application/json")
-		if err := json.NewEncoder(w).Encode(registry); err != nil {
-			logrus.WithError(err).WithField("registry", registry).Error("failed to encode page")
+		if err := json.NewEncoder(w).Encode(graph); err != nil {
+			logrus.WithError(err).WithField("graph", graph).Error("failed to encode graph")
 		}
+	})
+
+	handler.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		_, err := graphService.Get(ctx)
+		if err != nil {
+			logrus.WithError(err).Error("Error loading registry data")
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		_, err = fmt.Fprintf(w, "OK")
+		logrus.WithError(err).Error("failed to write response")
 	})
 
 	return handler
