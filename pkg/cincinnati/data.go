1d27c180d88f6c01f1e35ce3d7f3af73140317ea
Hongkai Liu <hongkailiu@users.noreply.github.com>
2026-04-07 08:28:54 -0400

    Fix a typo

--- pkg/cincinnati/data.go
+++ pkg/cincinnati/data.go
@@ -15,7 +15,7 @@ import (
 	"k8s.io/apimachinery/pkg/util/yaml"
 )
 
-// TODO: use graph-data whlile forming the graph, instead of adding first and removing later
+// TODO: use graph-data while forming the graph, instead of adding first and removing later
 
 type BlockedEdge struct {
 	RemovedEdge
